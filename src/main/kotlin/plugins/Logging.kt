e9dafbdc5c5bad6ce6b0b3e93418ba07df682de7
averen <avery@averen.dev>
2023-06-29 17:29:49 -0600

    configure logging

--- src/main/kotlin/plugins/Logging.kt
+++ src/main/kotlin/plugins/Logging.kt
@@ -19,11 +19,12 @@ package plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
-import org.slf4j.event.Level
 
 fun Application.configureLogging() {
     install(CallLogging) {
-        level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        format { call ->
+            "${call.response.status()} | ${call.processingTimeMillis()}ms | " +
+                    "${call.request.local.remoteAddress} | ${call.request.httpMethod.value} ${call.request.path()}"
+        }
     }
 }
